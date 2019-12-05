315021ca17693f1c4fdb3e16be79c44eeb413a50
Veerendra <veerendra2@github.com>
2019-12-05 21:44:09 +0530

    added install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "[*] Installing Dependency Packages"
+sudo apt-get update && sudo apt-get install curl feh xclip i3lock ngrep -y
+for x in .aliases .bash_profiles .bash_prompt .bashrc .curlrc .dockerfunctions .exports .functions .path .screenrc;
+do
+  echo "[*] Downloading to $HOME/$x"
+  curl -qo $HOME/$x https://raw.githubusercontent.com/veerendra2/dotfiles/master/$x > /dev/null 2>&1
+done
+bash
