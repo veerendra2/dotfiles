ce55b6af6df86d7aee39885fa7d388f4d675a33a
Veerendra <veerendra2@github.com>
2021-08-29 17:50:12 +0200

    [ADD] Aliase and fix banner

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "[*] Installing Dependency Packages"
-sudo apt-get update && sudo apt-get install curl feh xclip i3lock ngrep pv openssl -y
+sudo apt-get update && sudo apt-get install curl feh xclip i3lock ngrep pv openssl python3-pip -y
+pip3 install spotify-cli-linux
 for x in .aliases .bash_profiles .bash_prompt .bashrc .curlrc .dockerfunctions .exports .functions .path .screenrc;
 do
   echo "[*] Downloading to $HOME/$x"
