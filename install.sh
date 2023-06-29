3cba8991de9514ef3d2ed515a6e9752cef47e77f
Veerendra <8393701+veerendra2@users.noreply.github.com>
2023-06-29 21:42:09 +0200

    Backup

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-
+for file in $(shell find $(CURDIR) -name ".*" -not -name ".gitignore" -not -name ".git" -not -name ".config" -not -name ".github" -not -name ".*.swp" -not -name ".gnupg"); do \
+		f=$$(basename $$file); \
+		ln -sfn $$file $(HOME)/$$f; \
+	done;
