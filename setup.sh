80152bf85909cf109dcbaab9220b8d7ecaf4c664
veerendra2 <veerendra2@github.com>
2025-09-18 12:38:04 +0200

    Revert setup.sh

--- setup.sh
+++ setup.sh
@@ -2,34 +2,51 @@
 
 set -e
 
+# List of dotfiles directories
 declare -a dotfiles=(
   "bash"
   "curl"
   "git"
   "screen"
 )
-declare -a dotfiles_no_folding=(
-  ".config"
-  ".vim"
-)
 
-# A function to handle the stowing process
-function stow_dotfiles() {
-    local action=${1}
+if [[ "$1" == "--help" || "$1" == "-h" ]]; then
+  echo "Usage: $0 [option]"
+  echo
+  echo "Options:"
+  echo "  -i, --install      Install dotfiles (default)"
+  echo "  -r, --re-stow      Re-stow dotfiles (update symbolic links)"
+  echo "  -d, --delete       Delete dotfiles (uninstall)"
+  echo "  -h, --help         Show this help message"
+  exit 0
+fi
+
+ACTION=${1:-"-i"}
+
+case "$ACTION" in
+  "-d"|"--delete")
     pushd ~/projects/dotfiles > /dev/null
-
     for pkg in "${dotfiles[@]}"; do
-        stow "${action}" -t "${HOME}" "$pkg"
+        stow -D -t "${HOME}" "$pkg"
     done
-    for pkg in "${dotfiles_no_folding[@]}"; do
-        stow "${action}" -t "${HOME}/${pkg}" --no-folding "$pkg"
+    stow -D -t "${HOME}/.config" --no-folding .config
+    stow -D -t "${HOME}/.vim" --no-folding .vim
+    echo "[.] Dotfiles uninstalled!"
+    exit 0
+    ;;
+
+  "-r"|"--re-stow")
+    pushd ~/projects/dotfiles > /dev/null
+    for pkg in "${dotfiles[@]}"; do
+        stow -R -t "${HOME}" "$pkg"
     done
-    popd > /dev/null
-}
+    stow -R -t "${HOME}/.config" --no-folding .config
+    stow -R -t "${HOME}/.vim" --no-folding .vim
+    echo "[*] Dotfiles re-stowed!"
+    exit 0
+    ;;
 
-ACTION=${1:-"stow"}
-
-if [[ "$1" == "-i" ]]; then
+  "-i"|"--install")
     case "$(uname -s)" in
       "Darwin")
         brew install stow openssl starship
@@ -40,40 +57,35 @@ if [[ "$1" == "-i" ]]; then
         curl -sS https://starship.rs/install.sh | sh
         ;;
     esac
-    ACTION="stow"
-fi
 
-case "$ACTION" in
-  "-d"|"--delete")
-    stow_dotfiles "-D"
-    echo "[.] Dotfiles uninstalled!"
-    ;;
-  "-r"|"--re-stow")
-    stow_dotfiles "-R"
-    echo "[*] Dotfiles re-stowed!"
-    ;;
-  "stow")
     mkdir -p "${HOME}"/{projects,.config,.vim}
+
     pushd "${HOME}/projects" > /dev/null
+
+    # Clone dotfiles repo if not exists
     if [[ ! -d "dotfiles"  ]]; then
         ssh-keyscan github.com >> ~/.ssh/known_hosts
         git clone https://github.com/veerendra2/dotfiles.git
     fi
+
     pushd dotfiles > /dev/null
-    git pull > /dev/null
+
+    # Just to make sure get latest changes in case script runs
+    # in already cloned repo
+    git pull
+
+    # Create empty files to put machine specific/custom dotfiles
+    # manually if needed. These extra dotfiles are not tracked in git
     touch {bash/.extra,git/.extra-gitconfig}
-    stow_dotfiles
+
+    for pkg in "${dotfiles[@]}"; do
+      stow -t "${HOME}" "$pkg"
+    done
+    stow -t "${HOME}/.config" --no-folding .config
+    stow -t "${HOME}/.vim" --no-folding .vim
     echo "[*] Dotfiles installed!"
     ;;
-  "--help"|"-h")
-    echo "Usage: $0 [option]"
-    echo
-    echo "Options:"
-    echo "  -i      Install dependency packages and stow dotfiles"
-    echo "  -r      Re-stow dotfiles"
-    echo "  -d      Delete dotfiles"
-    echo "  -h      Show this help message"
-    ;;
+
   *)
     echo "Invalid option: $1"
     echo "Run '$0 --help' for usage."
